6f4e616c6e39e5e1691f4c598dd1053ecff3b02b
Hakkimhong12 <kimhongs200@gmail.com>
2025-02-08 15:58:05 +0700

    'update'

--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,22 @@
 import sys
-from src.logger import logging  # Ensure logging is correctly imported
+from src.logger import logger
 
-def error_message_detail(error, error_detail):
-    exc_type, exc_value, exc_tb = error_detail.exc_info()
+def error_message_detail(error, error_detail: sys):
+
+    _, _, exc_tb = sys.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
+    
     return error_message
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail=sys):
+
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail)
+        logger.error(self.error_message)  # Log the error when exception is created
         
     def __str__(self):
         return self.error_message
--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,20 @@ import logging
 import os 
 from datetime import datetime
 
-logs_path = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_path, exist_ok=True)
+def setup_logger():
+    
+    logs_path = os.path.join(os.getcwd(), "logs")
+    os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+    LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+    LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    level=logging.INFO,
-    format="[%(asctime)s] - %(lineno)d - %(name)s - %(levelname)s - %(message)s"
-)
+    logging.basicConfig(
+        filename=LOG_FILE_PATH,
+        format="[%(asctime)s] %(lineno)d - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO
+    )
+
+    return logging.getLogger(__name__)
+
+logger = setup_logger()
